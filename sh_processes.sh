bdef990cf15369a4b06381e65bf97aa1da16be2b
jol79 <jolhuck@gmail.com>
2020-11-03 20:50:07 +0000

    script that prints the processes running on the system

--- sh_processes.sh
+++ sh_processes.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# Displays the process running on the system for every
-# 30 seconds, but only for 3 times
+# Displays the process running on the system
 # 
-# for loop will be executed 3 times:
-for r in 1 2 3
 do
 	#see every process on the system
 	echo "**************************** x^x^x ****************************"
 	ps -e
 	echo "**************************** x^x^x ****************************"
-	#sleep for 30 seconds
-	sleep 3
         # clean
 done
